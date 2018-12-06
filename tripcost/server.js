f86456d9b5d54672f4353e48461ec6836983a3d0
DEV\lucas.picollo <lucas.picollo@audaces.com>
2018-12-06 15:46:02 -0200

    crud implemented

--- tripcost/server.js
+++ tripcost/server.js
@@ -18,19 +18,58 @@ mongo.connect(url, (err, client) => {
 app.use(express.json())
 
 app.post('/trip', (req, res) => {
-
+  const { name } = req.body;
+  trips.insertOne({ name: name }, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ err: err });
+      return;
+    }
+    console.log(result);
+    res.status(200).json({ ok: true });
+  });
 });
 
 app.get('/trips', (req, res) => {
-
+  trips.find().toArray((err, items) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ err: err });
+      return;
+    }
+    res.status(200).json({ trips: items });
+  })
 });
 
 app.post('/expense', (req, res) => {
-
+  expenses.insertOne(
+    {
+      trip: req.body.trip,
+      date: req.body.date,
+      amount: req.body.amount,
+      category: req.body.category,
+      description: req.body.description
+    },
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.send(500).json({ err: err });
+        return;
+      }
+      res.status(200).json({ ok: true });
+    }
+  )
 });
 
 app.get('/expenses', (req, res) => {
-
+  expenses.find().toArray((err, items) => {
+    if (err) {
+      console.error(err);
+      res.send(500).json({ err: err });
+      return;
+    }
+    res.status(200).json({ expenses: items });
+  });
 });
 
 
